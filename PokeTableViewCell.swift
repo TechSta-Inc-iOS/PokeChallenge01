a5549f13d7c6bb0b555a3bae4b0997baa12559a8
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-10 01:42:29 +0900

    不要な行とコードを削除

--- PokeTableViewCell.swift
+++ PokeTableViewCell.swift
@@ -8,53 +8,24 @@
 import UIKit
 
 final class PokeTableViewCell: UITableViewCell {
-
-    
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var pokeImageView: UIImageView!
-    
     @IBOutlet weak var hpLabel: UILabel!
     @IBOutlet weak var attackLabel: UILabel!
     @IBOutlet weak var defenseLabel: UILabel!
-    
-    
     @IBOutlet weak var specialAttackLabel: UILabel!
     @IBOutlet weak var specialDefenseLabel: UILabel!
     @IBOutlet weak var speedLabel: UILabel!
     
-    
     func configure(pokemon: Pokemon) {
         numberLabel.text = "No:\(pokemon.id)"
         nameLabel.text = pokemon.name.japanese
-        
         hpLabel.text = "HP\(pokemon.status.hp)"
         attackLabel.text = "こうげき\(pokemon.status.attack)"
         defenseLabel.text = "ぼうぎょ\(pokemon.status.defense)"
-        
         specialAttackLabel.text = "とくこう\(pokemon.status.specialAttack)"
         specialDefenseLabel.text = "とくぼう\(pokemon.status.specialDefense)"
         speedLabel.text = "すばやさ\(pokemon.status.speed)"
-        
-        
     }
-    
-   
-    
-    
-    
-    
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }

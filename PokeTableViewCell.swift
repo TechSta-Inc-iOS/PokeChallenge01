e0b918c0493175df3c7203995660631f75f934b8
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-10 22:46:52 +0900

    空白追加。制約の修正

--- PokeTableViewCell.swift
+++ PokeTableViewCell.swift
@@ -21,11 +21,11 @@ final class PokeTableViewCell: UITableViewCell {
     func configure(pokemon: Pokemon) {
         numberLabel.text = "No:\(pokemon.id)"
         nameLabel.text = pokemon.name.japanese
-        hpLabel.text = "HP\(pokemon.status.hp)"
-        attackLabel.text = "こうげき\(pokemon.status.attack)"
-        defenseLabel.text = "ぼうぎょ\(pokemon.status.defense)"
-        specialAttackLabel.text = "とくこう\(pokemon.status.specialAttack)"
-        specialDefenseLabel.text = "とくぼう\(pokemon.status.specialDefense)"
-        speedLabel.text = "すばやさ\(pokemon.status.speed)"
+        hpLabel.text = "HP \(pokemon.status.hp)"
+        attackLabel.text = "こうげき \(pokemon.status.attack)"
+        defenseLabel.text = "ぼうぎょ \(pokemon.status.defense)"
+        specialAttackLabel.text = "とくこう \(pokemon.status.specialAttack)"
+        specialDefenseLabel.text = "とくぼう \(pokemon.status.specialDefense)"
+        speedLabel.text = "すばやさ \(pokemon.status.speed)"
     }
 }
